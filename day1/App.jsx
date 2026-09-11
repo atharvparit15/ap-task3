00881c37c7184660850aab169f57419205d34890
Atharv Pandurang Parit <atharvparit800@gmail.com>
2026-09-11 22:28:14 +0530

    Update App.jsx

--- day1/App.jsx
+++ day1/App.jsx
@@ -1,10 +1,10 @@
 function App() {
   return (
     <div>
-      <h1>My Task Tracker</h1>
-      <p>Welcome to my React project!</p>
+      <h1>My React App</h1>
+      <p>Welcome to React!</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
