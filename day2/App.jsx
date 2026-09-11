80a8c1da62c63e4b272a65883a07eb4963649d30
Atharv Pandurang Parit <atharvparit800@gmail.com>
2026-09-11 22:29:39 +0530

    Update App.jsx

--- day2/App.jsx
+++ day2/App.jsx
@@ -1,13 +1,16 @@
-import Greeting from './Greeting'
+function Welcome({ name }) {
+  return <h2>Welcome, {name}!</h2>;
+}
 
 function App() {
   return (
     <div>
-      <h1>My Task Tracker</h1>
-      <Greeting name="Omkar" />
-      <p>Learning React Components and Props</p>
+      <h1>Components & Props</h1>
+
+      <Welcome name="Atharv" />
+      <Welcome name="React Intern" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
