61dcf17e92f91057193d57364e0c4507f56a95a4
Atharv Pandurang Parit <atharvparit800@gmail.com>
2026-09-11 22:33:02 +0530

    Update TaskList.jsx

--- day7/TaskList.jsx
+++ day7/TaskList.jsx
@@ -1,19 +1,24 @@
-import TaskItem from './TaskItem'
+import TaskItem from "./TaskItem";
 
-function TaskList({ tasks, onComplete, onDelete }) {
+function TaskList({ tasks, toggleTask, deleteTask }) {
   return (
-    <ul className="mt-6 space-y-3">
-      {tasks.map((task, index) => (
-        <TaskItem
-          key={index}
-          task={task}
-          index={index}
-          onComplete={onComplete}
-          onDelete={onDelete}
-        />
-      ))}
-    </ul>
-  )
+    <div>
+      {tasks.length === 0 ? (
+        <p className="text-center text-gray-500">
+          No tasks yet.
+        </p>
+      ) : (
+        tasks.map((task) => (
+          <TaskItem
+            key={task.id}
+            task={task}
+            toggleTask={toggleTask}
+            deleteTask={deleteTask}
+          />
+        ))
+      )}
+    </div>
+  );
 }
 
-export default TaskList
+export default TaskList;
